c66f261d2627615b8804236352c6d3198e6393db
Nepire <37898081+Nepire@users.noreply.github.com>
2019-01-10 20:10:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 git clone https://github.com/nepire/Pwngdb.git ~/Pwngdb
-chmod +x ~/Pwngdb/pwndbg/setup.sh && ~/Pwngdb/pwndbg/setup.sh
+chmod +x ~/Pwngdb/pwndbg/setup.sh && cd ~/Pwngdb/pwndbg/ && ./setup.sh
 cp ~/Pwngdb/.gdbinit ~/.gdbinit
