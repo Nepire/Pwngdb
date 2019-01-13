ad852a7d7dc03d29aa80281d45ee2bc0ebe6f46e
断竹残赋 <37898081+Nepire@users.noreply.github.com>
2019-01-13 19:46:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 export LC_ALL=C
-
 git clone https://github.com/nepire/Pwngdb.git ~/Pwngdb
-sudo apt-get install -y python-setuptools
-sudo apt-get install -y python3-setuptools
+sudo apt-get install -y python-setuptools python3-setuptools
 cd ~/Pwngdb/pwndbg/
 chmod a+x ./setup.sh && ./setup.sh
 cp ~/Pwngdb/.gdbinit ~/.gdbinit
